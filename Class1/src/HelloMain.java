745479fafc3f2710c1c9d983828602c1c241c4df
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2022-09-01 11:34:05 -0400

    first code

---
+++ Class1/src/HelloMain.java
@@ -0,0 +1,18 @@
+public class HelloMain {
+    /**
+     * Explain what the function/Method is doing
+     * 
+     * Input
+     * 
+     * @param args
+     *             Output
+     */
+    public static void main(String[] args) {
+        // using \ before a string symbol makes it ignore it and print it 
+        System.out.println("Hello, World!\\n"+"Hello my Class\t"+"Noooooo"+"\n");
+        System.out.println(53+1);// Sum of two nnumbers
+        System.out.println("53"+1); //Turn 1 to string then combine it with 53 to produce 531
+        System.out.println(Integer.parseInt("53")+1); //Turn string to int then sum them
+
+    }
+}
