2a84344a631947a475b06ca9ceae035e0b278deb
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2022-09-08 11:19:43 -0400

    homework

---
+++ Class3/src/ProPlayer.java
@@ -0,0 +1,48 @@
+public class ProPlayer {
+    public static void main(String[] args) throws Exception {
+
+        // Variables an holders for args
+        int age = Integer.parseInt(args[0]);
+        String name = args[1];
+        double m = Double.parseDouble(args[2]);
+
+        // place holder for printing variables
+        String proStatus, sport;
+
+        // if (age >= 0 && age < 10){
+        // proStatus = "youth";
+        // }
+        // else if (age >= 10 && age < 30){
+        // proStatus = "Pro";
+        // }
+        // else {
+        // proStatus = "Coach";
+        // }
+        // Method 2 : use methods
+        proStatus = myStatus(age);
+
+        //The reason that I didn't check the values for 2<= is that it is included in first if 
+        if (m >= 0 && m < 2) {
+            sport = "Football";
+        } else if (m < 5) {
+            sport = "Vball";
+        } else if (m < 6) {
+            sport = "Tennis";
+        } else {
+            sport = "basketball";
+        }
+
+        System.out.println("Hi " + name + " you should play " + sport + " and your level is " + proStatus);
+    }
+
+    private static String myStatus(int age) {
+        if (age >= 0 && age < 10) {
+            return "youth";
+        } else if (age >= 10 && age < 30) {
+            return "Pro";
+        } else {
+            return "Coach";
+        }
+    }
+
+}
