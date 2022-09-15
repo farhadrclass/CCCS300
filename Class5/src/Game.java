e9d313b8023490d1e164045ab0cd73d99d87bf9b
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2022-09-15 11:29:55 -0400

    Game

--- Class5/src/Game.java
+++ Class5/src/Game.java
@@ -1,13 +1,28 @@
-import java.util.Scanner;  // Import the Scanner class
+import java.util.Scanner; // Import the Scanner class
 
 public class Game {
     public static void main(String[] args) {
 
-    Scanner myScn = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter an Int");
+        Scanner myScn = new Scanner(System.in); // Create a Scanner object
+        System.out.println("Enter an Int");
 
-    int mychoice = myScn.nextInt();  // Read user input
-    System.out.println("my Int  is: " + mychoice);  // Output user input
+        int userInput = myScn.nextInt(); // Read user input
+        int numTry = 0;
+        int answer = 12;
+        while (numTry < 4 ) {
+            System.out.println("your anwer is Int  is: " + userInput); // Output user input
+            if (answer == userInput) {
+                break;
+            }
+            numTry++;
+            System.out.println("Wrong, please try agian");
+            userInput = myScn.nextInt();
+        }
+        if (numTry < 5) {
+            System.out.println("WINNER");
+        } else {
+            System.out.println("Loser");
+        }
     }
 
 }
